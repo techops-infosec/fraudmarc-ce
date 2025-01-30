e902bb9e1c6cb95f9362db52a6cb3395800c653c
Effy Elden <effye@thoughtworks.com>
2025-01-30 15:53:29 +1100

    Add config

--- frontend/src/environments/environment.common.ts
+++ frontend/src/environments/environment.common.ts
@@ -1,6 +1,6 @@
-const apiEndpoint = '';
-const userPoolId = '';
-const userPoolClientId = '';
+const apiEndpoint = 'https://jd498qw9n7.execute-api.ap-southeast-2.amazonaws.com/';
+const userPoolId = 'ap-southeast-2_Uz9nmDnL8';
+const userPoolClientId = '3qkv2vd5s9glotvq4bmd2ba7bs';
 
 const apiPath: string = '/api';
 
